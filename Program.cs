82711280c9a5a20d7847ede192bb772961f8568d
jiink <jacob.j.carlson03@gmail.com>
2023-11-07 09:35:43 -0800

    Working serial reading

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static SerialPort port = new SerialPort("COM9", 115200, Parity.None, 8, StopBits.One);
+        static void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Show all the incoming data in the port's buffer
+            Console.WriteLine(port.ReadExisting());
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            port.Handshake = Handshake.None;
+            port.DataReceived += port_DataReceived;
+            port.Open();
+            Console.ReadLine();
+        }
+
+        
+    }
+}
